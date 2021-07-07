10c7ad46aa9e41f5f06405da18c3a5a4d8614236
“NargizaRysbek” <r.nakosya@gmail.com>
2021-07-07 15:31:44 +0600

    created webpack and added pages,store

---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import "./App.less";
+const App = () => <div className="App">Nargiz Group Azamat team Aigerim</div>;
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "react-dom";
+import {createStore} from 'redux'
+import  {Provider} from 'react-redux'
+
+import App from "./App.jsx";
+import { rootReducer } from "./store/rootReducer.js";
+
+const store = createStore(rootReducer)
+
+render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
