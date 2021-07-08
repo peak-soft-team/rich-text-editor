7896e2b7a9c5a97c14df7ed7c957ddfb3d500a16
Azamat Maratov <aza2001.05.16.gmail.com>
2021-07-08 16:41:15 +0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
-import React from "react";
-import "./App.less";
-const App = () => <div className="App">Nargiz Group Azamat team Aigerim, Orozbek</div>;
-export default App;
+import React from 'react'
+import './App.less'
+const App = () => {
+	return (
+		<div className='App'>
+			Nargiz Group Azamat team Aigerim, Orozbek
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+				Ratione sunt nisi, exercitationem eveniet, repellendus ipsum
+				consequuntur ducimus pariatur repellat reiciendis nam quis cum.
+				Facere, fugiat sed error veritatis laudantium quos.
+			</p>
+		</div>
+	)
+}
+export default App
