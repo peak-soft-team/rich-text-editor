70ddc8ebb390479cae3622433c9f706a7af68d01
Orozobek <oroz1111omurkulov@gmail.com>
2021-07-19 13:26:02 +0600

    fixed buges, was deleted and  reinstalled webpack again

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./App.less";
 import { Login } from "./pages/Login";
 
-// import { MainLayout } from "./pages/MainLayout/index.jsx";
+import { MainLayout } from "./pages/MainLayout/index.jsx";
 const App = () => (
   <div className="App">
-      <Login/>
-    {/* <MainLayout className="animate__animated animate__pulse" /> */}
+      {/* <Login/> */}
+    <MainLayout className="animate__animated animate__pulse" />
   </div>
 );
 export default App;
